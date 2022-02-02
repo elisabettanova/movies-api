7a181aae5675f7ee715183857f30375d14eed419
Anna Lisbona <arisubona@gmail.com>
2022-02-02 18:47:38 +0100

    Authentication controller and user routes

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+
+//@access public
+exports.login = (req, res, next) => {};
+
+exports.logout = (req, res, next) => {};
+
+//@access public
+exports.register = (req, res, next) => {};
+
+exports.getUser = (req, res, next) => {};
--- controllers/catalog.js
+++ controllers/catalog.js
@@ -14,10 +14,40 @@ exports.getAllMovies = (req, res, next) => {
 };
 
 //@access public
-exports.getByCategory = (req, res, next) => {};
+exports.getByCategory = (req, res, next) => {
+  const category = req.params.category;
+  Movie.find({ category: { $regex: category, $options: "i" } })
+    .then((movies) =>
+      res.status(200).json({
+        success: true,
+        movies,
+      })
+    )
+    .catch((error) => next(error));
+};
 
 //@access public
-exports.searchByTitle = (req, res, next) => {};
+exports.searchByTitle = (req, res, next) => {
+  //const title = req.params.title;
+  Movie.find({ title: { $regex: req.query.title, $options: "i" } })
+    .then((movies) =>
+      res.status(200).json({
+        success: true,
+        movies,
+      })
+    )
+    .catch((error) => next(error));
+};
 
 //@access public
-exports.getMovieById = (req, res, next) => {};
+exports.getMovieById = (req, res, next) => {
+  const id = req.params.id;
+  Movie.findById(id)
+    .then((movie) =>
+      res.status(200).json({
+        success: true,
+        movie,
+      })
+    )
+    .catch((error) => next(error));
+};
